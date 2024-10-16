a1f4d23b76f06854c90441a940df5c973da38272
setuukka <seppatuukka@gmail.com>
2024-10-16 10:31:06 +0300

    made a triangle

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     #print("Starting asteroids!")
     while True:
@@ -10,7 +14,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+
+        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)       
+        player.draw(screen)
         pygame.display.flip()
+        return_time = clock.tick(60)
+        dt = return_time / 1000
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
 
