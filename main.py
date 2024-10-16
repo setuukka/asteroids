20f0bb6d2ee23d2dbed971004baaf5aeafa22997
setuukka <seppatuukka@gmail.com>
2024-10-16 21:05:38 +0300

    Rotating player

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import pygame
 from constants import *
-from player import *
-from circleshape import *
+from player import Player
+#from circleshape import *
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    dt = 0
     #print("Starting asteroids!")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
+
         screen.fill("black")
-
-
-        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)       
         player.draw(screen)
+       
+
         pygame.display.flip()
-        return_time = clock.tick(60)
-        dt = return_time / 1000
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 from circleshape import CircleShape
 import pygame
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y, PLAYER_RADIUS)
         self.rotation = 0
+        print(f"Initial rotation: {self.rotation}") #debug print
 
     # in the player class
     def triangle(self):
@@ -19,3 +20,20 @@ class Player(CircleShape):
     def draw(self, screen):
         #print("We are in player.py draw function")
         pygame.draw.polygon(screen, "white", self.triangle(),2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        print(f"Rotation after update: {self.rotation}") #debug print
+        #print(f"rotation {self.rotation}, turn speed {PLAYER_TURN_SPEED}, dt {dt}")
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            print("Rotating left") #Debug print
+            self.rotate(-dt)  # Rotate left
+
+        if keys[pygame.K_d]:
+            print("Rotating right") #Debug print
+
+            self.rotate(dt)   # Rotate right
